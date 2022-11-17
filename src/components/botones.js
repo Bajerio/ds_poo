eb5dd55f8aab3a143252fcfbea4a53e0a4b19079
Bajerio <clound125@gmail.com>
2022-11-17 17:24:54 +0100

    Nueva rama de ultima actualizacion 17/11/2022

--- src/components/botones.js
+++ src/components/botones.js
@@ -9,144 +9,399 @@ const btn_pelo = document.querySelector("#pelo_perso")
 const btn_pelo_ojos = document.querySelector("#pelo_ojos_perso")
 
 
+const pSex = document.querySelector(".panel_sexo")
+const btn_sex_men = document.querySelector("#sub_btn_hombre")
+const btn_mujer = document.querySelector("#sub_btn_mujer")
 
-let mensaje1 = document.querySelector("#message1")
-let mensaje2 = document.querySelector("#message2")
+
+const parra = document.querySelector("#parrafo")
 
 
-console.log(mensaje1.value)
-console.log(mensaje2.value)
-
-const btn = document.querySelector("#boton")
-//Elementos de lista
-
-//const lista_sexo = document.querySelector(".sexo_list")
-// const hombre = document.querySelector("#hom")
-// const mujer = document.querySelector("#muj")
-
-// //console.log(lista_sexo.value)
-// console.log(hombre.value)
-// console.log(mujer.value)
-
-// let text_area = document.querySelector("#area")
-
-const caracteristicas = "top=200,left=700,height=300,width=500,resizable=no,scrollbars=yes"
+const caracteristicas = "top=200,left=700,height=300,width=500,resizable=0,scrollbars=yes"
 
 const sexo_lista = ["Hombre","Mujer"]
 
+let ventana
 
 
 function elegir_sexo(){
-
-    btn_sexo.addEventListener("click", function(){
-
-      
-       const ventana = window.open('','nuevaVentana',caracteristicas)
-       ventana.document.body.style.backgroundColor = "red"
-
-       lista_sexo.innerHTML = ventana
-
-
-        if(!ventana) console.log("Ventana no cargada")
     
+    btn_sexo.addEventListener("click", function(){
+       interfaz_sexo()
+      // botones_internos_sexo()
     })
-    }
+}
 
 elegir_sexo()
 
-function elegir_clase(){
 
-    btn_clase.addEventListener("click", function(){
+
+function botones_internos_sexo(){
+
+
+    ventana = window.open('','nuevaVentana',caracteristicas)
+    btn_sexo.disabled = true
+
+    // ventana.appendChild(pSex)
+     ventana.document.write(parra.outerHTML)
+     //ventana.document.write(pSex.outerHTML)
+    ventana.document.body.style.backgroundColor = "red"
+
+    ventana.onbeforeunload = function(event){
+
+        btn_sexo.disabled = false
+        return true
+   
+       }
+
+
+}
+
+
+
+function interfaz_sexo(){
+
+
+    //Creacion de parrafo en el body
+
+
       
-        const ventana = window.open('','nuevaVentana',caracteristicas)
-        if(!ventana) console.log("Ventana no cargada")
+    ventana = window.open('','nuevaVentana',caracteristicas)
+    btn_sexo.disabled = true
+
+
+    //Elementos dinamicos HTML
+     
+
+    let divElement = document.createElement("div")
+    let divElementEmpty = document.createElement("div")
+
+
+    let inputMen = document.createElement("input")
+    inputMen.setAttribute('type','button')
+    inputMen.setAttribute('value','Hombre')
+    inputMen.setAttribute('id','sub_btn_hombre')
+
+    let prueba_btn = document.querySelector("#sub_btn_hombre")
+
+    let campo = document.createElement("input")
+    campo.setAttribute('type','text')
+
+   
+
+            
+      const inputWoman = document.createElement("input")
+    //  let inputWoman = document.createElement("button")
+     inputWoman.setAttribute('type','button')
+     inputWoman.setAttribute('value','Mujer')
+
+     //ESTILOS CSS
+
+     divElement.style.cssText += 'display:flex; justify-content:center; flex-direction:column;align-items:center;margin-top:100px;'
+     inputMen.style.cssText += 'align-items:center;background:black;font-size:30px;color:red;font-family:Montserrat sans-serif'
+     inputWoman.style.cssText += 'align-items:center;background:black;font-size:30px;color:red;font-family:Montserrat sans-serif'
+ 
+
     
-    })
+    //AÑADIR A LA VENTANA
+
+     divElement.appendChild(inputMen)
+     divElement.appendChild(divElementEmpty)
+     divElement.appendChild(inputWoman)
+     divElement.appendChild(campo)
+     ventana.document.write(divElement.outerHTML)
+     ventana.document.body.style.backgroundColor = "red"
+
+
+
+//ACCIONES DE BOTONES DENTRO DE LA VENTANA
+
+    //METODO ASPA BOTON VENTANA CERRAR
+
+    ventana.onbeforeunload = function(event){
+
+     btn_sexo.disabled = false
+     return true
+
     }
 
-elegir_clase()
+    //BOTON HOMBRE
+
+   // btn_hombre(inputMen)
+
+  
+        inputMen.addEventListener('click',function(e){ //Undefined
+
+            try{
+                console.log("Entra en prueba_btn")
+                campo.focus()
+            }
+
+            catch(e){
+                alert(e.message)
+            }
+
+            })
+        
+    
+
+/*
+inputMen.addEventListener('click',(e)=>{ 
+
+    console.log("Entra hombre")
+    document.write("TEXTO")
+   
+
+})
+
+inputWoman.addEventListener('click',function(){
+
+
+    console.log("Entra mujer")
+
+
+})
+
+
+   divElement.inputMen.addEventListener('click',function(){ //Undefined
+
+
+        console.log("Entra en prueba_btn")
+        campo.focus()
+
+
+
+    })
+
+ 
+
+    prueba_btn.addEventListener('click',(e)=>{ 
+
+        console.log("Entra hombre")
+        btn_sexo.value = valorMen
+        campo.focus()
+
+    })
+    inputMen.addEventListener('click',(e)=>{ 
+
+        console.log("Entra hombre")
+        btn_sexo.value = valorMen
+        campo.focus()
+
+    })
+
+    inputMen.addEventListener('click',function(){ 
+
+        console.log("Entra hombre")
+        btn_sexo.value = valorMen
+        campo.focus()
+
+    })
+
+    inputWoman.addEventListener('click',function(){
+
+        console.log("Entra mujer")
+        btn_sexo.value = valorMen
+
+    })
+    
+    */
+ }
+
+
+ function btn_hombre(elementoListener){
+
+    let elementoListenerd = document.createElement("input")
+
+    elementoListenerd = elementoListener
+
+    console.log("Entra en btn_hombre")
+
+    elementoListenerd.addEventListener('click',(e)=>{ 
+
+        console.log("Entra hombre")
+        btn_sexo.value = valorMen
+        campo.focus()
+        
+        return elementoListenerd
+    })
+    
+ }
+
+
+function elegir_clase(){
+
+    let divElement = document.createElement("div")
+
+
+    let inputMen = document.createElement("input")
+    inputMen.setAttribute('type','button')
+    inputMen.setAttribute('value','Hombre')
+    inputMen.setAttribute('id','sub_btn_hombre')
+
+    divElement.appendChild(inputMen)
+
+        btn_clase.addEventListener("click", function(e){
+
+        
+
+            ventana = window.open('','nuevaVentana',caracteristicas)
+            btn_clase.disabled = true
+            ventana.document.body.style.backgroundColor = "red"
+            ventana.document.write(divElement.outerHTML)
+
+         
+            // if(e.target && e.target.id == 'sub_btn_hombre'){
+            //     console.log("Entra en prueba_btn")
+            // }
+    
+       
+    
+        inputMen.addEventListener("click", (e)=>{
+
+        if(e.target && e.target.id == 'sub_btn_hombre'){
+            console.log("Entra en prueba_btn")
+            }
+        })
+
+    }) 
+    
+    ventana.onbeforeunload = function(event){
+    
+        btn_clase.disabled = false
+        return true
+
+    }
+    
+    
+    // inputMen.addEventListener("click", function(){
+        
+        //     console.log("Entra en prueba_btn")
+            
+        // })
+}
+
+//elegir_clase()
 
 function elegir_regalo(){
 
     btn_regalo.addEventListener("click", function(){
 
-       //console.log("Entra en elegir_sexo()")
-
-
-        const ventana = window.open('','nuevaVentana',caracteristicas)
-
-        if(!ventana) console.log("Ventana no cargada")
+        ventana = window.open('','nuevaVentana',caracteristicas)
+        btn_regalo.disabled = true
+        ventana.document.body.style.backgroundColor = "red"
+ 
+        ventana.onbeforeunload = function(event){
+ 
+            btn_regalo.disabled = false
+         return true
+ 
+        }
     
     })
     }
 
     elegir_regalo()
 
-function elegir_fisico(){
+  /*  ventana = window.open('','nuevaVentana',caracteristicas)
+    btn_clase.disabled = true
+    ventana.document.body.style.backgroundColor = "red"
 
-    btn_fisico.addEventListener("click", function(){
+    ventana.onbeforeunload = function(event){
 
-       //console.log("Entra en elegir_sexo()")
+     btn_clase.disabled = false
+     return true
 
+    }*/
 
-        const ventana = window.open('','nuevaVentana',caracteristicas)
+    function elegir_fisico(){
 
-        if(!ventana) console.log("Ventana no cargada")
+        btn_fisico.addEventListener("click", function(){
     
-    })
-    }
-
-    elegir_fisico()
-
-function elegir_cara(){
-
-    btn_cara.addEventListener("click", function(){
-
-       //console.log("Entra en elegir_sexo()")
-
-
-        const ventana = window.open('','nuevaVentana',caracteristicas)
-
-        if(!ventana) console.log("Ventana no cargada")
+            ventana = window.open('','nuevaVentana',caracteristicas)
+            btn_fisico.disabled = true
+            ventana.document.body.style.backgroundColor = "red"
+        
+            ventana.onbeforeunload = function(event){
+        
+                btn_fisico.disabled = false
+             return true
+        
+            }
+        
+        })
+        }
     
-    })
-    }
-
-elegir_cara()
-
-function elegir_pelo(){
-
-    btn_pelo.addEventListener("click", function(){
-
-       //console.log("Entra en elegir_sexo()")
+        elegir_fisico()
 
 
-        const ventana = window.open('','nuevaVentana',caracteristicas)
 
-        if(!ventana) console.log("Ventana no cargada")
-    
-    })
-    }
+        function elegir_cara(){
 
-elegir_pelo()
+            btn_cara.addEventListener("click", function(){
+        
+                ventana = window.open('','nuevaVentana',caracteristicas)
+                btn_cara.disabled = true
+                ventana.document.body.style.backgroundColor = "red"
+            
+                ventana.onbeforeunload = function(event){
+            
+                    btn_cara.disabled = false
+                 return true
+            
+                }
+            
+            })
+            }
+        
+        elegir_cara()
 
-function elegir_pelo_ojos(){
 
-    btn_pelo_ojos.addEventListener("click", function(){
+        function elegir_pelo(){
 
-       //console.log("Entra en elegir_sexo()")
+            btn_pelo.addEventListener("click", function(){
+        
+                ventana = window.open('','nuevaVentana',caracteristicas)
+                btn_pelo.disabled = true
+                ventana.document.body.style.backgroundColor = "red"
+            
+                ventana.onbeforeunload = function(event){
+            
+                    btn_pelo.disabled = false
+                 return true
+            
+                }
+            
+            })
+            }
+
+            elegir_pelo()
+
 
             
+        function elegir_pelo_ojos(){
 
-        const ventana = window.open('','nuevaVentana',caracteristicas)
+            btn_pelo_ojos.addEventListener("click", function(){
 
-        if(!ventana) console.log("Ventana no cargada")
-    
-    })
-    }
+                ventana = window.open('','nuevaVentana',caracteristicas)
+                btn_pelo_ojos.disabled = true
+                ventana.document.body.style.backgroundColor = "red"
+            
+                ventana.onbeforeunload = function(event){
+            
+                    btn_pelo_ojos.disabled = false
+                 return true
+            
+                }
+            
+            })
+            }
 
-elegir_pelo_ojos()
+        elegir_pelo_ojos()
 
+/*
+
+//ACEPTAR Y PERSONALIZAR
 
 
 function btn_accept(){
@@ -154,16 +409,16 @@ function btn_accept(){
     btn_aceptar.addEventListener("click", function(){
 
       //  console.log("Entra en btn_accept()")
+      let ventana
 
-
-        const ventana = window.open('','nuevaVentana',caracteristicas)
+         ventana = window.open('','nuevaVentana',caracteristicas)
 
         if(!ventana) console.log("Ventana no cargada")
     
     })
     }
 
-btn_accept()
+btn_accept()*/
 
 
 // function controlErrores(value){
